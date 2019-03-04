f6fbf94bf0d06895d018288109f1b22f00fcc006
TabEsc <1471748102@qq.com>
2019-03-04 18:52:44 +0800

    Struts框架

--- src/com/login/LoginAction.java
+++ src/com/login/LoginAction.java
@@ -5,16 +5,13 @@ import com.opensymphony.xwork2.ActionSupport;
 public class LoginAction extends ActionSupport {
 
 
-
 	private static final long serialVersionUID = 1L;
 	private String name;
 	private String password;
 	 
 	public String execute() throws Exception {
- 		//简单应用
 		//System.out.println("账号:" + getName());
 		//System.out.println("密码:"+ getPassword());
- 		//gitee上传
 		if (getName().equals("") || getName() == null) {
 			return "error";
 		} else {
