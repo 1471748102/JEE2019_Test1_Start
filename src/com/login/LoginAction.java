8ca8f07f3ee5f431faaef4021e66e5d628977e19
TabEsc <1471748102@qq.com>
2019-03-04 18:27:11 +0800

    简单添加注释

--- src/com/login/LoginAction.java
+++ src/com/login/LoginAction.java
@@ -16,7 +16,7 @@ public class LoginAction extends ActionSupport {
  		//简单应用
 		//System.out.println("账号:" + getName());
 		//System.out.println("密码:"+ getPassword());
- 
+ 		//gitee上传
 		if (getName().equals("") || getName() == null) {
 			return "error";
 		} else {
